399b058b3daea834f5966c3fc4f0b85f140e3267
junnplus <junnplus@gmail.com>
2016-04-08 21:04:48 +0000

    Env from os

--- weixin/config.py
+++ weixin/config.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 
 class Config(object):
 
-    SECRET_KEY = 'secret_key'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'secret_key')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
     # WEIXIN
-    WEIXIN_TOKEN = 'token'
-    WEIXIN_CONSUMER_KEY = 'wx6ebca2c3abe568af'
-    WEIXIN_CONSUMER_SECRET = '15cbe6cbd0c40eea496ca8ca0d2c8a7e'
+    WEIXIN_TOKEN = os.environ.get('WEIXIN_TOKEN', 'token')
+    WEIXIN_CONSUMER_KEY = os.environ.get('WEIXIN_CONSUMER_KEY',
+                                         'wx6ebca2c3abe568af')
+    WEIXIN_CONSUMER_SECRET = os.environ.get('WEIXIN_CONSUMER_SECRET',
+                                            '15cbe6cbd0c40eea496ca8ca0d2c8a7e')
 
 
 class DevelopmentConfig(Config):
@@ -26,7 +30,7 @@ class TestingConfig(Config):
 class ProductionConfig(Config):
 
     # SENTRY
-    SENTRY_DSN = 'https://3880902278bb48699e9cdd99aa2da7d5:a97e1346bdae42dca912ff90d6c8c3bd@app.getsentry.com/73089'  # noqa
+    SENTRY_DSN = os.environ.get('SENTRY_DSN', 'https://3880902278bb48699e9cdd99aa2da7d5:a97e1346bdae42dca912ff90d6c8c3bd@app.getsentry.com/73089')  # noqa
 
 
 config = {
